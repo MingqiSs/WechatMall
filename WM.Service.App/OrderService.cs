1e163ce3791cde22ce68998c202cd402e77b204e
向明其 <354063818@qq.com>
2020-03-29 14:27:45 +0800

    update api

--- WM.Service.App/OrderService.cs
+++ WM.Service.App/OrderService.cs
@@ -9,6 +9,7 @@ using WM.Service.Domain.Interface;
 using X.Models.WMDB;
 using System.Linq;
 using WM.Service.App.Dto.WebDto.RQ;
+using WM.Service.Domain.Entities;
 
 namespace WM.Service.App
 {
@@ -19,10 +20,12 @@ namespace WM.Service.App
     {
         private readonly X.IRespository.DBSession.IWMDBSession _ibll;
         private readonly IUserDomainService _userDomainService;
-        public OrderService(X.IRespository.DBSession.IWMDBSession ibll, IUserDomainService userDomainService)
+        private readonly IProductDomainService _productDomainService;
+        public OrderService(X.IRespository.DBSession.IWMDBSession ibll, IUserDomainService userDomainService, IProductDomainService productDomainService)
         {
             _ibll = ibll;
             _userDomainService = userDomainService;
+            _productDomainService = productDomainService;
         }
         /// <summary>
         /// 获取购物车信息
@@ -37,15 +40,15 @@ namespace WM.Service.App
                 TotalPrice = 0,
                 OrderCradinfo = new List<OrderCradinfoRP> { },
             };
-            var orderCard= _ibll.wm_order_card.Where(q => q.UID == uid&&q.DataStauts==(byte)EnumDataStatus.Enable).First();
+            var orderCard= _ibll.wm_order_card.Where(q => q.UID == uid&&q.DataStauts==(byte)DataStatus.Enable).First();
             if (orderCard != null)
             {
                 var order = _ibll.DB.Queryable<wm_order_card_info, wm_product>((c, p) => new object[] {
                                             JoinType.Inner,c.ProductID==p.ID,
                                       })
                             .Where((c, p) => c.Order_CardID == orderCard.ID
-                                    && c.DataStatus == (byte)EnumDataStatus.Enable
-                                    && p.DataStatus == (byte)EnumDataStatus.Enable)
+                                    && c.DataStatus == (byte)DataStatus.Enable
+                                    && p.DataStatus == (byte)DataStatus.Enable)
                             .Select((c, p) => new OrderCradinfoRP
                             {
                                 ProductID = c.ProductID,
@@ -59,17 +62,110 @@ namespace WM.Service.App
            
             return Result(result);
         }
-        ///// <summary>
-        ///// 修改商品到购物车
-        ///// </summary>
-        ///// <param name="uid"></param>
-        ///// <param name="rq"></param>
-        ///// <returns></returns>
-        //public ResultDto<bool> ModifyOrderCard(string uid, ModifyOrderCardRQ rq)
-        //{
-        //    var user = _userDomainService.GetUserByUID(uid);
-        //    if (user == null) return Result<OrderCradRP>(ResponseCode.sys_token_invalid, "获取用户信息错误");
+        /// <summary>
+        /// 添加商品到购物车
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <param name="rq"></param>
+        /// <returns></returns>
+        public ResultDto<bool> AddShoppingCard(string uid, AddShoppingCardRQ rq)
+        {
+            var user = _userDomainService.GetUserByUID(uid);
+            if (user == null) return Result<bool>(ResponseCode.sys_token_invalid, "获取用户信息错误");
+            var isSave = false;
+            var product=_productDomainService.GetProductById(rq.ProductID);
+            if (product == null)
+            {
+                return Result<bool>(ResponseCode.sys_param_format_error, "商品不存在");
+            }
+           var cardId = _ibll.wm_order_card.Where(q => q.UID == uid && q.DataStauts == (byte)DataStatus.Enable)
+                                       .Select(a=>a.ID).First();
+            if (cardId == 0)
+            {
+                cardId = _ibll.wm_order_card.AddReturnId(new wm_order_card
+                {
+                    UID=uid,
+                    Checked=false,
+                    CreateTime=DateTime.Now,
+                    DataStauts=(byte)DataStatus.Enable,
+                });
+            }
+            isSave = _ibll.wm_order_card_info.Add(new wm_order_card_info
+            {
+                Order_CardID = cardId,
+                DataStatus = (byte)DataStatus.Enable,
+                ProductID = product.ID,
+                Product_Num = rq.ProductNumber,
+                CreateTime = DateTime.Now,
+            });
+            return Result(isSave);
+        }
 
-        //}
+        /// <summary>
+        /// 修改商品到购物车
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <param name="rq"></param>
+        /// <returns></returns>
+        public ResultDto<bool> ModifyShoppingCard(string uid, ModifyShoppingCardRQ rq)
+        {
+            var user = _userDomainService.GetUserByUID(uid);
+            if (user == null) return Result<bool>(ResponseCode.sys_token_invalid, "获取用户信息错误");
+            var isSave = false;
+            var product = _productDomainService.GetProductById(rq.ProductID);
+            if (product == null)
+            {
+                return Result<bool>(ResponseCode.sys_param_format_error, "商品不存在");
+            }
+            var cardId = _ibll.wm_order_card.Where(q => q.UID == uid && q.DataStauts == (byte)DataStatus.Enable)
+                                        .Select(a => a.ID).First();
+            if (cardId == 0)
+            {
+                cardId = _ibll.wm_order_card.AddReturnId(new wm_order_card
+                {
+                    UID = uid,
+                    Checked = false,
+                    CreateTime = DateTime.Now,
+                    DataStauts = (byte)DataStatus.Enable,
+                });
+            }
+            var cardinfo = _ibll.wm_order_card_info.Where(q => q.Order_CardID == cardId && q.ProductID == rq.ProductID && q.DataStatus == (byte)DataStatus.Enable).First();
+            if (cardinfo != null)
+            {
+
+                cardinfo.Product_Num = rq.Product_Num;
+
+                isSave = _ibll.wm_order_card_info.Update(cardinfo);
+            }
+            return Result(isSave);
+        }
+        /// <summary>
+        /// 修改商品到购物车
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <param name="rq"></param>
+        /// <returns></returns>
+        public ResultDto<bool> RemoveShoppingCard(string uid, RemoveShoppingCardRQ rq)
+        {
+            var user = _userDomainService.GetUserByUID(uid);
+            if (user == null) return Result<bool>(ResponseCode.sys_token_invalid, "获取用户信息错误");
+            var isSave = false;
+            var cardId = _ibll.wm_order_card.Where(q => q.UID == uid && q.DataStauts == (byte)DataStatus.Enable)
+                                        .Select(a => a.ID).First();
+            if (cardId == 0)
+            {
+                return Result<bool>(ResponseCode.sys_param_format_error, "购物车没有商品");
+            }
+            foreach (var productID in rq.ProductIDs)
+            {
+                var cardinfo = _ibll.wm_order_card_info.Where(q => q.Order_CardID == cardId && q.ProductID == productID && q.DataStatus == (byte)DataStatus.Enable).First();
+                if (cardinfo != null)
+                {
+                    cardinfo.DataStatus = (byte)DataStatus.Delete;
+                    isSave = _ibll.wm_order_card_info.Update(cardinfo);
+                }
+            }
+            return Result(isSave);
+        }
     }
 }
