43f5f3fb9355f939f913b9115ca6fd3a1310f5a2
向明其 <xiangmingqi@taichengtech.com>
2020-04-03 16:34:53 +0800

    add Manager.MVC

---
+++ WM.Manager.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WM.Manager.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
